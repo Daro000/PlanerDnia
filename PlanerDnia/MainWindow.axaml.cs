63c99e5ceca2d51ad1a2c0d0ebaa0079bef1399b
daro <dfghjk@email.com>
2025-03-18 08:31:22 +0100

    utworzenie klasy ZadanieButton

--- PlanerDnia/MainWindow.axaml.cs
+++ PlanerDnia/MainWindow.axaml.cs
@@ -8,4 +8,18 @@ public partial class MainWindow : Window
     {
         InitializeComponent();
     }
+
+    public class ZadanieButton{
+        public string Nazwa{get;set;}
+        public string Kategoria{get;set;}
+        public bool CzyUkonczone{get;set;}
+
+        ZadanieButton(string nazwa, string kategoria)
+        {
+            Nazwa = nazwa;
+            Kategoria = kategoria;
+            CzyUkonczone = false;
+        }
+        
+    }
 }
