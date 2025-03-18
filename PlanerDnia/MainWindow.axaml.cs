1e83fb4655ce5c57bdafa5f7464de3f521f519f5
daro <dfghjk@email.com>
2025-03-18 09:00:05 +0100

    Utworzenie klasy Stworz_button_Click do utworzenia zadan

--- PlanerDnia/MainWindow.axaml.cs
+++ PlanerDnia/MainWindow.axaml.cs
@@ -1,12 +1,18 @@
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace PlanerDnia;
 
 public partial class MainWindow : Window
 {
+    public ObservableCollection<ZadanieButton> ListaZadan { get; set; }
     public MainWindow()
     {
         InitializeComponent();
+        ListaZadan = new ObservableCollection<ZadanieButton>();
+        Stworz.Click += Stworz_button_Click;
     }
 
     public class ZadanieButton{
@@ -14,12 +20,30 @@ public partial class MainWindow : Window
         public string Kategoria{get;set;}
         public bool CzyUkonczone{get;set;}
 
-        ZadanieButton(string nazwa, string kategoria)
+        public ZadanieButton(string nazwa, string kategoria)
         {
             Nazwa = nazwa;
             Kategoria = kategoria;
             CzyUkonczone = false;
         }
         
+    }
+
+    private void Stworz_button_Click(object sender, RoutedEventArgs e)
+    {
+        var valueZadanie = Zadanie.Text;
+        var valueprzedmiot = (RZadania.SelectedItem as ComboBoxItem) ?.Content?.ToString() ?? "Nie wybrane";
+        
+        if (!string.IsNullOrEmpty(valueZadanie) && !string.IsNullOrEmpty(valueprzedmiot))
+        {
+            ZadanieButton newZadanieButton= new ZadanieButton(valueZadanie, valueprzedmiot);
+            ListaZadan.Add(newZadanieButton);
+            Zadanie.Clear();
+            RZadania.SelectedIndex = -1;
+
+            UpdateSummary();
+        }
+    }
+
     }
 }
